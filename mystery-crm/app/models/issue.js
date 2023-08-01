d4791da948b29d3850740e93999c155fb97d1007
rydev-genesys <115428365+rydev-genesys@users.noreply.github.com>
2023-08-01 16:12:24 -0700

    20230801

--- mystery-crm/app/models/issue.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    createdDate: DS.attr('string'),
-    title: DS.attr('string'),
-    status: DS.attr('string'),
-    description: DS.attr('string')
-});
